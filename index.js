6fa247ea1fd03b759957dd08df768a802e0b14d9
Vinicius-G-F <vinicius666_@live.com>
2023-10-13 14:58:01 -0300

    fazendo o upload inicial da api

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { Telegraf } from 'telegraf';
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const port = 3000;
+
+app.use(cors())
+app.use(express.json());
+
+app.post('/enviar-mensagem', (req, res) => {
+  const mensagem = req.body.mensagem;
+
+  const token = process.env.TOKEN;
+  const chatId = process.env.CHAT_ID;
+
+  try{
+    const bot = new Telegraf(token)
+    bot.telegram.sendMessage(chatId, mensagem)
+    res.status(200).json({message: "mensagem enviada"})
+  } catch(error){
+    res.status(500).json({ error: 'Erro ao enviar a mensagem' });
+  }
+
+  
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Express em execução na porta ${port}`);
+});
